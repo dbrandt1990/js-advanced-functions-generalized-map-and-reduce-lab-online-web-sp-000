d5b5627135899cae33241704710f2863945a8a13
dbrandt1990 <dvbrandt90@gmail.com>
2021-05-26 13:19:22 -0600

    commit tree 21b30fe54e4c5284656724224c0fe9ee22374dc6

--- index.js
+++ index.js
@@ -7,11 +7,14 @@ const map = (arr, fn) => {
     return newArr
 }
 
-const reduce = (arr, fn, statrVal) => {
-    arr.forEach(item => {
-        let total
-        let thing = fn(item, statrVal)
-        console.log(thing)
-        total += thing
-    })
+const reduce = (arr, fn, startVal) => {
+    let acc = (!!startVal) ? startVal : arr[0]
+    // let i = (!!startVal) ? 0 : 1
+
+    for (let i = 0; i < arr.length; i++) {
+
+        acc = fn(arr[i], acc)
+
+    }
+    return acc
 }
